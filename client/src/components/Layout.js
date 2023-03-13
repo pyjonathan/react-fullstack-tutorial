ace394426f26025db4e4c3eefc3927212af1ccd5
pyjonathan <jonathan.r.buchanan@gmail.com>
2023-03-12 22:45:39 -0400

    Added Global Context

---
+++ client/src/components/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import AuthBox from "./AuthBox";
+import { useGlobalContext } from "../context/GlobalContext";
+
+const Layout = () => {
+  const { fetchingUser } = useGlobalContext();
+
+  return fetchingUser ? (
+    <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  ) : (
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route exact path="/" element={<AuthBox />} />
+        <Route exact path="/register" element={<AuthBox register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Layout;
